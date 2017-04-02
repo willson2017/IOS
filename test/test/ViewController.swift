f6db242fbf047ddbc8d8643b2d69ea24b67ef956
Xinyu Qu <qux04@unl102954-m.uniad.unitec.ac.nz>
2017-04-03 10:53:16 +1200

    add polygon

--- test/test/ViewController.swift
+++ test/test/ViewController.swift
@@ -25,6 +25,53 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    func degree2radian(a:CGFloat)->CGFloat{
+        let ret = CGFloat(M_PI) * a/180
+        return ret
+    }
+    
+    
+    func polyPoints(sides: Int, x:CGFloat, y:CGFloat, radius:CGFloat, adjustment:CGFloat = 0)->[CGPoint]
+    {
+        let angle = degree2radian(a: 360/CGFloat(sides))
+        let cx = x
+        let cy = y
+        let r = radius
+        var i = sides
+        var points = [CGPoint]()
+        
+        while points.count <= sides{
+            let xpo = cx - r * cos(angle * CGFloat(i) + degree2radian(a: adjustment))
+            let ypo = cy - r * sin(angle * CGFloat(i) + degree2radian(a: adjustment))
+            points.append(CGPoint(x:xpo, y:ypo))
+            i -= 1
+        }
+        return points
+        
+    }
+    
+    
+    func polygonPath(x: CGFloat, y:CGFloat, radius:CGFloat, sides:Int, offset:CGFloat)->CGPath
+    {
+        let path = CGMutablePath()
+        let points = polyPoints(sides: sides, x: x, y: y, radius: radius, adjustment: offset)
+        let cpg = points[0]
+        path.move(to: cpg)
+        
+        for p in points{
+            path.addLine(to: p)
+        }
+        
+        path.closeSubpath()
+        
+        return path
+        
+    }
+    
+    
+    
     @IBAction func operation(_ sender: UIPanGestureRecognizer) {
         
         if sender.state == .began
@@ -57,10 +104,28 @@ class ViewController: UIViewController {
 
             case 3: //line
                 
-                var linePath = UIBezierPath()
+                let linePath = UIBezierPath()
                 linePath.move(to: startPoint)
                 linePath.addLine(to: endPoint)
                 layer?.path = linePath.cgPath
+            
+            case 4: //triangle
+                let trianglepath = polygonPath(x: startPoint.x, y:startPoint.y, radius: translation.x/3, sides: 3, offset: 0)
+                let tPath = UIBezierPath(cgPath:trianglepath)
+                
+               layer?.path = tPath.cgPath
+            
+            case 5: //diamond
+                let diamondPath = polygonPath(x: startPoint.x, y:startPoint.y, radius: translation.x/4, sides: 4, offset: 0)
+                let diamondBezierpath = UIBezierPath(cgPath:diamondPath)
+                
+                layer?.path = diamondBezierpath.cgPath
+
+            case 6: //star
+                let starPath = polygonPath(x: startPoint.x, y:startPoint.y, radius: translation.x/10, sides: 6, offset: 2)
+                let starBezierPath = UIBezierPath(cgPath:starPath)
+                
+                layer?.path = starBezierPath.cgPath
                 
             default:
                 print("no idea")
@@ -74,7 +139,7 @@ class ViewController: UIViewController {
 
     @IBAction func globalConfig(_ sender: UIButton) {
         
-        configG = 3 //oval
+        configG = 6
     }
 
 }
